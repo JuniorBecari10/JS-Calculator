2dd1ad7d49cf231b7232cc79369587375539cb47
Antônio Carlos <juniorbecari@gmail.com>
2022-03-20 14:36:10 -0300

    Now the clear button works

--- calculator/www/js/script.js
+++ calculator/www/js/script.js
@@ -1,12 +1,18 @@
 var num_input = document.getElementsByClassName("num")[0];
 var pre_result = document.getElementsByClassName("pre-result")[0];
 
+const clearBtn = document.getElementById("clear");
+
+clearBtn.onclick = clear;
+
 function addNum(s) {
     num_input.value += s;
     pre_result.innerHTML = calc(num_input.value)
 }
 
 function clear() {
+    console.log("a");
+    
     num_input.value = "";
     pre_result.innerHTML = "";
 }
