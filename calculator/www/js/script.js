afa59000b270b56cb8643e1520910ae4c173726e
Antônio Carlos <juniorbecari@gmail.com>
2022-03-20 14:38:26 -0300

    Now the equal button also works

--- calculator/www/js/script.js
+++ calculator/www/js/script.js
@@ -2,8 +2,10 @@ var num_input = document.getElementsByClassName("num")[0];
 var pre_result = document.getElementsByClassName("pre-result")[0];
 
 const clearBtn = document.getElementById("clear");
+const equalBtn = document.getElementById("equal");
 
 clearBtn.onclick = clear;
+equalBtn.onclick = equal;
 
 function addNum(s) {
     num_input.value += s;
@@ -11,9 +13,14 @@ function addNum(s) {
 }
 
 function clear() {
-    console.log("a");
-    
     num_input.value = "";
+    pre_result.innerHTML = "";
+}
+
+function equal() {
+    let pres = pre_result.innerHTML;
+    
+    num_input.value = pres;
     pre_result.innerHTML = "";
 }
 
